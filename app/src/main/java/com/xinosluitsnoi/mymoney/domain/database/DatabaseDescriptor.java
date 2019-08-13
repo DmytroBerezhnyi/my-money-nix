1c41c3e7579dced0cd0f08edf24d85f010e8a1f8
Dmytro Berezhnyi <dmytro.berezhnyi1@gmail.com>
2019-08-13 22:37:56 +0300

    IC-7-5 DatabaseDescriptor

---
+++ app/src/main/java/com/xinosluitsnoi/mymoney/domain/database/DatabaseDescriptor.java
@@ -0,0 +1,59 @@
+package com.xinosluitsnoi.mymoney.domain.database;
+
+import android.provider.BaseColumns;
+
+public final class DatabaseDescriptor {
+
+    private DatabaseDescriptor() {
+
+    }
+
+    public static final class TransactionEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "transaction";
+
+        public static final String TOTAL = "total";
+
+        public static final String TITLE = "title";
+
+        public static final String CATEGORY = "category_id";
+
+        public static final String DATE = "date";
+
+        public static final String TYPE = "type";
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY, " +
+                        TITLE + " TEXT NOT NULL," +
+                        TOTAL + " REAL NOT NULL," +
+                        "FOREIGN KEY(" + CATEGORY + ") REFERENCES " +
+                        TABLE_NAME + "(" + _ID + ")," +
+                        DATE + " INTEGER(8) NOT NULL," +
+                        TYPE + " INTEGER(1) NOT NULL);";
+
+        public static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static final class CategoryEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "category";
+
+        public static final String NAME = "name";
+
+        public static final String COLOR = "color";
+
+        public static final String TYPE = "type";
+
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY, " +
+                        NAME + " TEXT NOT NULL," +
+                        COLOR + " REAL NOT NULL," +
+                        TYPE + " INTEGER(1) NOT NULL);";
+
+        public static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+}
